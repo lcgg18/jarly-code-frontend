1686a9480f53e27e16c2bdc1770bfd2bea012353
Luis C Guerra <85770481+lcgg18@users.noreply.github.com>
2021-12-21 11:28:02 -0500

    nuevo

--- src/__test__/button.test.js
+++ src/__test__/button.test.js
@@ -4,6 +4,7 @@ import { render, screen, cleanup } from '@testing-library/react';
 
 afterEach(cleanup);
 
+
 it('renders okay', () => {
     render(<ButtonLoading text='hola' loading={false} disabled={false} />);
     expect(screen.getByTestId('button-loading')).toBeInTheDocument();
